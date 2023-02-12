ced349ba783919e69f2895f479a83388018d1b9d
SMwebdeveloper <samandarwebdeveloper@gamil.com>
2023-02-12 22:50:41 +0500

    Finish base framework functional

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-
+import {Excel} from '@/components/excel/Excel'
+import {Header} from '@/components/header/Header'
+import {Toolbar} from '@/components/toolbar/Toolbar'
+import {Table} from '@/components/table/Table'
+import {Formula} from '@/components/formula/Formula'
 import './scss/index.scss'
-console.log("Working!");
-console.log('hello');
+
+const excel = new Excel('#app', {
+    components:[Header, Toolbar,Formula, Table]
+})
+
+excel.render()
